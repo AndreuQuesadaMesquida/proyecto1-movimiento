96a3a21a17f572795d8f85f95392209783c471d1
AndreuQuesadaMesquida <andreuquesadamesquida@gmail.com>
2023-10-23 10:46:18 +0200

    coche

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] private GameObject camera;
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float lateralSpeed = 5f;
+
+    [SerializeField] private Vector3 offset = new Vector3(0, 7, -10);
+
+    private float rotationInput;
+
+    private float horizontalInput;
+    private float verticalInput;
+    
+    
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        rotationInput = Input.GetAxis("Rotation");
+
+
+        transform.Translate(Vector3.forward * speed *  Time.deltaTime * verticalInput);
+
+        transform.Translate(Vector3.right * lateralSpeed * Time.deltaTime * horizontalInput);
+
+        
+
+        camera.transform.position = transform.position + offset;
+
+    }
+}
